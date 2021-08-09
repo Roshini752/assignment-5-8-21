ae2f297ff72ad5f60a28e11deb5f83619988c31e
rosini <roshinipeyyala@xyz.com>
2021-08-09 16:37:27 +0530

    sec_ass_9/8/21

--- pattern_printing_while.c
+++ pattern_printing_while.c
@@ -3,21 +3,27 @@
 int main()
 {
     int i=0,j,n=5;
-    while(++i<=n)       //i=1 i=2 i=3 i=4 i=5
+    int k;
+    while(++i<=n)       // i=1 i=2 i=3
     {
-        printf("%d",i);
-        printf("%c",47);         // / / / / /
-        j=i;      // j=1 j=2 j=3 j=4 j=5
+        for (k = 1; k <= n - i; ++k) // k=1;k<=4
+        {
+            printf(" ");                              
+        }
+        
+        printf("%c",47);         
+        j=i;                 // j=1 j=2 j=3
         while(j!=1)     
         {
+            
             j--; 
-            printf("%d",j);       //   j=1  j=2,j=1 j=3,j=2,j=1 j=4,j=3,j=2,j=1
-            printf(" ");  
+                 
+            printf("  ");  
             
         }
-        printf("%c\n",92);    // \ \ \ \ 
-       
+        printf("%c\n",92);  
+        printf("\n");
+               
     }
-
     return 0;
 }
